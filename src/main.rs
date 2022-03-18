d3f04bbb516df95200be5f88efa6ac0f986f9c8b
tontsa28 <xdman7917@gmail.com>
2022-03-18 20:50:21 +0200

    feat: Implement tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+use rocket::http::Status;
+use rocket::routes;
+
+#[get("/")]
+async fn check(pid: &String) -> Status {
+    if Path::new(pid).exists() {
+        println!("TS3 server running");
+        Status::Ok
+    }
+    else {
+        println!("TS3 server not running");
+        Status::NotFound
+    }
+}
+
+async fn main() {
+    dotenv::dotenv().expect("ERROR: Failed to load .env file");
+    let port = dotenv::var("PORT").expect("ERROR: Port missing");
+    let pid = dotenv::var("PIDFILE").expect("ERROR: Pidfile missing");
+
+    rocket::ignite().mount("/", routes(check(&pid)).launch());
 }
