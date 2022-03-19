05317616b766325b5f0545356f781f5ec8737e85
tontsa28 <miika@tontsa.fi>
2022-03-19 14:29:01 +0200

    style: Comment code

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,29 @@ use std::path::Path;
 use rocket::http::Status;
 use rocket::routes;
 
+// Root endpoint
 #[get("/")]
 async fn check() -> Status {
-    let pid = dotenv::var("PIDFILE").expect("ERROR: Pidfile missing");
+    // Get pid file path from .env file
+    let pid = dotenv::var("PIDFILE").expect("ERROR: PIDFILE path missing");
+
+    // If pid file exists
     if Path::new(&pid).exists() {
+        // Return code 200
         println!("TS3 server running");
         Status::Ok
     }
+    // If pid file does not exist
     else {
+        // Return code 404
         println!("TS3 server not running");
         Status::NotFound
     }
 }
 
+// Main function
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().expect("ERROR: Failed to load .env file");
+    // Set root endpoint to check function and launch Rocket
     rocket::build().mount("/", routes![check]).launch().await.unwrap();
 }
