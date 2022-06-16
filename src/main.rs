de589457f68d8ecb195ca3722eb30943d5394fb3
tontsa28 <miika@tontsa.fi>
2022-06-16 13:26:21 +0300

    feat: Use actix-web instead of Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,35 @@
-#[macro_use] extern crate rocket;
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
 use std::path::Path;
-use rocket::http::Status;
-use rocket::routes;
-use tracing::info;
+use tracing::{info, warn};
 
 // Root endpoint
 #[get("/")]
-async fn check() -> Status {
-    // Get pid file path from .env file
-    let pid = dotenv::var("PIDFILE").expect("ERROR: PIDFILE path missing");
+async fn check() -> impl Responder {
+    // Get pid file variable specified in the .env file
+    let pid = dotenv::var("PID_FILE").expect("ERROR: PID_FILE path missing");
 
-    // If pid file exists
+    // Check if the pid file exists
     if Path::new(&pid).exists() {
-        // Return code 200
+        // Return code 200 OK
         info!("Process running");
-        Status::Ok
-    }
-    // If pid file does not exist
-    else {
-        // Return code 404
-        info!("Process not running");
-        Status::NotFound
+        HttpResponse::Ok()
+    } else {
+        // Return code 404 NOT FOUND
+        warn!("Process not running");
+        HttpResponse::NotFound()
     }
 }
 
 // Main function
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     // Initialize tracing subscriber
     tracing_subscriber::fmt::init();
 
-    // Set root endpoint to check function and launch Rocket
-    let _rocket = rocket::build().mount("/", routes![check]).launch().await?;
-    Ok(())
+    // Get address and port variables specified in the .env file
+    let address = dotenv::var("ADDRESS").expect("ERROR: Address missing");
+    let port = dotenv::var("PORT").expect("ERROR: Port missing").parse::<u16>().unwrap();
+
+    // Start the web server
+    HttpServer::new(|| App::new().service(check)).bind((address, port))?.run().await
 }
